70d5b18403209d6ffb7dbdea913264539b8072ef
Parveen Pasricha <184731702+ParveenPasricha@users.noreply.github.com>
2026-05-11 19:07:48 +0530

    Update Code

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import './index.css'
 import App from "./App";
 
-import { CartProvider } from "./components/CartContext";
+import { CartProvider } from "../src/Components/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
